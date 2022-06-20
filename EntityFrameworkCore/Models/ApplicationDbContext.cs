f979af84637fa48f6fce0e03d99bf6642c91054e
Tuğçe Eraslan <tugceeraslan@emekkitap.com>
2022-06-20 15:59:31 +0300

    + DbContext oluşturuldu.

---
+++ EntityFrameworkCore/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Models
+{
+    public class ApplicationDbContext:DbContext   // DbContext imiz tanıttıktan sonra DbSet lerimizi tanıtabiliriz
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+             :base(options)  // options ı base e gönderelim
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }  // Veritabanında bir tablo oluşturduk. Tabloya tamamen Products nesnesi üzerinden ulaşabiliriz.
+    }
+}
